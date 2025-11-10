b1df6afdbfc3844600fa52ddb57b005d5fd21b2e
ohjunseok[lab] <megaIT@megaIT>
2025-11-10 18:55:05 +0900

    [학원] - mybatis 설정 및 데이터베이스 연동 중 (내일 이어서 다시 할예정)

---
+++ src/main/java/com/sourjelly/springexample/database/UsedGoodsController.java
@@ -0,0 +1,17 @@
+package com.sourjelly.springexample.database;
+
+import org.springframework.stereotype.Controller;
+
+// Request, Response 관련 기능 수행
+
+@Controller
+public class UsedGoodsController {
+
+    // 중고 거래 게시글 모든 정보 response에 담기
+    public usedGoodsList(){
+
+        // 중고 거래 게시트 리스트 얻어오기 (이 친구가 할거 아님 다른친구한테 임무부여)
+
+    }
+
+}
---
+++ src/main/java/com/sourjelly/springexample/database/repository/UsedGoodsRepository.java
@@ -0,0 +1,17 @@
+package com.sourjelly.springexample.database.repository;
+
+import com.sourjelly.springexample.database.domain.UsedGoods;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+// Mybatis Framework
+@Mapper
+public interface UsedGoodsRepository {
+
+
+    // used_goods 모든 행 조회
+    public List<UsedGoods> selectUsedGoodsList();
+
+
+}
